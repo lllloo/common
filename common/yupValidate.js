dd742ac1eda27fe910a1b172185a29aaebeb2c23
Barney <asdf01618@gmail.com>
2023-07-06 21:44:54 +0800

    [fix]調整註解

--- common/yupValidate.js
+++ common/yupValidate.js
@@ -1,9 +1,23 @@
+import { Schema } from 'yup';
+
+/**
+ * 驗證 整個物件
+ * @param {Schema} schema 
+ * @param {Object} data 
+ * @returns {Promise}
+ */
 export function validate(schema, data) {
     return schema
         .validate(data, { abortEarly: false })
         .catch((err) => new Promise((resolve, reject) => reject(err.inner)));
 }
 
+/**
+ * 驗證 單個參數
+ * @param {Schema} schema 
+ * @param {Object} data 
+ * @returns {Promise}
+ */
 export function validateAt(schema, data, name) {
     return schema
         .validateAt(name, data)
